ab321a1ffb4a89e4b840f4e7b5c76ac7ba0e5826
Szymek <szymon.rzadca@gmail.com>
2018-05-30 09:53:52 +0200

    error with int input and working it around

--- functions.py
+++ functions.py
@@ -4,11 +4,15 @@ def python_food():
     left_margin = (width - len(text)) // 2
     print(" " * left_margin, text)
 
+
 def centre_text(text):
+    text = str(text)
     left_margin = (80 - len(text)) // 2
     print(" " * left_margin, text)
 
+
 # call the function
 centre_text("spam and eggs")
+centre_text(12)
 centre_text("spam, spam and eggs")
 centre_text("spam, spam, spam and spam")
