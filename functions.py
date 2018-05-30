5e81ee07a916e735b7ff8b64d15bfe67ffb8a575
Szymek <szymon.rzadca@gmail.com>
2018-05-30 10:32:20 +0200

    writing input to another file

--- functions.py
+++ functions.py
@@ -14,15 +14,24 @@ def centre_text(*args, sep=' '):
 
 
 # with open("centered", mode='w') as centered_file:
-    # call the function
-s1 = centre_text("spam and eggs")
-s2 = centre_text(12)
-s3 = centre_text("spam, spam and eggs")
-s4 = centre_text("spam, spam, spam and spam")
-s5 = centre_text("first", "second", 3, 4, "spam", sep=':')
-print(s1)
-print(s2)
-print(s3)
-print(s4)
-print(s5)
+# call the function
+# s1 = centre_text("spam and eggs")
+# s2 = centre_text(12)
+# s3 = centre_text("spam, spam and eggs")
+# s4 = centre_text("spam, spam, spam and spam")
+# s5 = centre_text("first", "second", 3, 4, "spam", sep=':')
+# print(s1)
+# print(s2)
+# print(s3)
+# print(s4)
+# print(s5)
 
+with open("menu", "w") as menu:
+    s1 = centre_text("spam and eggs")
+    print(s1, file=menu)
+    print(centre_text(12), file=menu)
+    s3 = centre_text("spam, spam and eggs")
+    print(s3, file=menu)
+    print(centre_text("spam, spam, spam and spam"), file=menu)
+    s5 = centre_text("first", "second", 3, 4, "spam", sep=':')
+    print(s5, file=menu)
